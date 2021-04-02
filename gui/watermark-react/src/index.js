1b7986b5e85a73d95ea8ccf1ee3381b1b1b55eab
cnissnzg <cnissnzg@126.com>
2021-04-02 19:25:06 +0800

    Save before ejecting

--- gui/watermark-react/src/index.js
+++ gui/watermark-react/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Base from './component/superbase'
-import Home from './home.js'
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Url} from './common/common';
+import Home from './home';
+import ProblemList from './component/problem/home'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+ReactDOM.render((
+  <Router>
+    <Switch>
+      <Route exact path={Url.homepage} component={Home}/>
+      <Route exact path={Url.problem.list} component={ProblemList}/>
+    </Switch>
+  </Router>
+),
   document.getElementById('root')
 );
 
-
-reportWebVitals();
