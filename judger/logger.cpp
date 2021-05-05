c0bcd827ab8d0759806ed3f32b6e237703e4b396
root <cnissnzg@126.com>
2021-05-06 02:07:27 +0800

    3

---
+++ judger/logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 2021/5/6.
+//
+
+#include "logger.h"
---
+++ judger/logger.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 2021/5/6.
+//
+
+#ifndef JUDGER_LOGGER_H
+#define JUDGER_LOGGER_H
+
+
+class logger {
+
+};
+
+
+#endif //JUDGER_LOGGER_H
