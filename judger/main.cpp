9ea6dd2f70cc805033f545fa439e9591fc184094
root <cnissnzg@126.com>
2021-05-05 23:32:10 +0800

    second

--- judger/main.cpp
+++ judger/main.cpp
@@ -1,9 +1,12 @@
 #include "properties.h"
+#include "problem.h"
 
 /*
  * judge (language)
  */
 int main(int argc,char* argv[]) {
     int lang = getLang(string(argv[1]));
+    problem problemInfo(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]),string(argv[5]),string(argv[6]),string(argv[7]));
+
     return 0;
 }
