94db6b0bbc9e07cf3d2f9e006528b7d5a99d3fc6
root <cnissnzg@126.com>
2021-05-05 22:59:11 +0800

    judger first

---
+++ judger/cmake-build-debug/CMakeFiles/judger.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/judger.dir/challenge.cpp.o"
+  "CMakeFiles/judger.dir/main.cpp.o"
+  "CMakeFiles/judger.dir/problem.cpp.o"
+  "judger"
+  "judger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/judger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ judger/main.cpp
@@ -0,0 +1,9 @@
+#include "properties.h"
+
+/*
+ * judge (language)
+ */
+int main(int argc,char* argv[]) {
+    int lang = getLang(string(argv[1]));
+    return 0;
+}
