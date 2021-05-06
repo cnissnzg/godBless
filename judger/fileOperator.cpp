7ccff17ae15d88325dc4e4093499627125f8105e
root <cnissnzg@126.com>
2021-05-07 04:05:58 +0800

    try 2

---
+++ judger/fileOperator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 2021/5/7.
+//
+
+#include "fileOperator.h"
---
+++ judger/fileOperator.h
@@ -0,0 +1,8 @@
+//
+// Created by root on 2021/5/7.
+//
+
+#ifndef JUDGER_FILEOPERATOR_H
+#define JUDGER_FILEOPERATOR_H
+
+#endif //JUDGER_FILEOPERATOR_H
